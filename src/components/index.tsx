e1dcc3eea5ff28962f54511553f2cda8f8b8c348
Vitor Silva <ksvitorsilva@gmail.com>
2020-08-27 15:41:15 +0100

    add anchors

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 // Components
 export { default as Container } from './container';
 export { default as Header } from './header';
+export { default as Link } from './link';
 export { default as ThemeSelector } from './theme-selector';
---
+++ src/components/link.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const A = styled.a`
+  font-size: 1.2em;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+type HeaderProps = {
+  href: string;
+  children: React.ReactNode;
+  target?: '_blank' | '_parent' | '_self' | ' _top	';
+};
+
+const Link = ({
+  href,
+  children,
+  target = '_blank',
+}: HeaderProps): JSX.Element => (
+  <A href={href} target={target}>
+    {children}
+  </A>
+);
+
+export default Link;
